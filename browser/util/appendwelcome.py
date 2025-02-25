12ef6c62eb300e1950110868bcef5597f30f2db2
zzs <hi@vio.vin>
2025-02-25 15:30:52 +0800

    style: disable welcome card

--- browser/util/appendwelcome.py
+++ browser/util/appendwelcome.py
@@ -4,20 +4,23 @@
 import sys
 import polib
 
-welcome = polib.pofile(sys.argv[1],
-                  autodetect_encoding=False,
-                  encoding="utf-8",
-                  wrapwidth=-1)
-coolui = polib.pofile(sys.argv[2],
-                  autodetect_encoding=False,
-                  encoding="utf-8",
-                  wrapwidth=78)
-# Filter out unnecessary strings of meta tags from html2po output
-for entry in welcome:
-    if 'html.head.meta' in entry.occurrences[0][0]:
-        continue
-    if entry.msgid == '':
-        continue
-    coolui.append(entry)
-coolui.metadata['Content-Type'] = 'text/plain; charset=UTF-8'
-coolui.save(sys.argv[2])
+sys.exit("appendwelcome.py is now a no-op.")
+
+
+# welcome = polib.pofile(sys.argv[1],
+#                   autodetect_encoding=False,
+#                   encoding="utf-8",
+#                   wrapwidth=-1)
+# coolui = polib.pofile(sys.argv[2],
+#                   autodetect_encoding=False,
+#                   encoding="utf-8",
+#                   wrapwidth=78)
+# # Filter out unnecessary strings of meta tags from html2po output
+# for entry in welcome:
+#     if 'html.head.meta' in entry.occurrences[0][0]:
+#         continue
+#     if entry.msgid == '':
+#         continue
+#     coolui.append(entry)
+# coolui.metadata['Content-Type'] = 'text/plain; charset=UTF-8'
+# coolui.save(sys.argv[2])
